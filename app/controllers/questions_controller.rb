feb6f5156606b254766a9d02e2c5a91ecd882c48
Jose Bello <bellojosema11@gmail.com>
2022-09-26 16:20:46 +0100

    Error with name tag on form solved

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,15 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:ask]
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', 'I do not care, get dressed and go to work!']
+
+    if @question == 'I am going to work'
+      @coach_answer = @answers[0]
+    elsif @question.end_with?('?')
+      @coach_answer = @answers[1]
+    else
+      @coach_answer = @answers[2]
+    end
   end
 end
